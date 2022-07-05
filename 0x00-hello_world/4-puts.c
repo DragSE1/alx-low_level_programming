3bae589f239c0d873a78cbe6d22d877962f87519
Victor B. Onakomaiya <bolu.onako.1000@gmail.comgggit config --global user.email bolu.onako.1000@gmail.com>
2022-07-04 17:53:27 -0700

    correction 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+
 /*
- * main- prints out sentence
+ * main- entry point
+ *
+ * Discription- prints sentence
  *
  * return:Always 0 (success)
  */
@@ -8,5 +11,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
