17f3dae070690b66b83a5f9223eb80a9f5e293f4
Victor B. Onakomaiya <bolu.onako.1000@gmail.comgggit config --global user.email bolu.onako.1000@gmail.com>
2022-07-19 09:15:22 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _memset - fills a pointer with a contant byte b for n times
+ * @s: pointer to string
+ * @b: constant byte
+ * @n: no. of times b is filled in
+ * @i: local variable
+ *
+ * Return:  s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; *(s + i) != \0; i++)
+	{
+		if (i < n)
+		{
+			*(s + i) = b;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+functions for more pointers, arrays, and strings
