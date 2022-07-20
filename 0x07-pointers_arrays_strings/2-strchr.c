82a1486910c724f88ea34a033c7eb16e62171137
Victor B. Onakomaiya <bolu.onako.1000@gmail.comgggit config --global user.email bolu.onako.1000@gmail.com>
2022-07-19 21:38:51 -0700

    2-strchr.c cont.1

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a char in a string
@@ -10,13 +11,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	long unsigned int i;
 
 	for (i = 0; i < sizeof(s); i++)
 	{
 		if (*(s + i) != c)
 		{
-			*(s + i) = NULL;
+			*(s + i) = '\0';
 		}
 		else if (*(s + i) == c)
 		{
