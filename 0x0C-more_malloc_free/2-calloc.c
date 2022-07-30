aca6781d2facbce3e88c4ea0ac6c9932b7373c52
Victor B. Onakomaiya <bolu.onako.1000@gmail.comgggit config --global user.email bolu.onako.1000@gmail.com>
2022-07-29 18:47:04 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - mallocs a several blocks of mem of same size
+ * @nmemb: number of blocks
+ * @size: size of all blocks
+ *
+ * Return: on condition
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *array;
+
+	array = malloc(nmemb * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	else if (nmemb ==  0 || size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (array);
+	}
+}
+
+
