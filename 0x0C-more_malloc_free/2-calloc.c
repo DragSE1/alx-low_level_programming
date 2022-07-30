2a249ef5fc7266ab5d417b64d6cd088c14ff697d
Victor B. Onakomaiya <bolu.onako.1000@gmail.comgggit config --global user.email bolu.onako.1000@gmail.com>
2022-07-29 19:35:22 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *array;
+	char *array;
 	unsigned int i;
 
 	array = malloc(nmemb * size);
@@ -29,7 +29,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		for (i = 0; i < (nmemb * size); i++)
 		{
-			array[i] = 0;
+			array[i] = '0';
 		}
 
 		return (array);
