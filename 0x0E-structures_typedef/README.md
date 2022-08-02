50e17747d6a13fa0ab0a18028a01998c758a37d0
Victor B. Onakomaiya <bolu.onako.1000@gmail.comgggit config --global user.email bolu.onako.1000@gmail.com>
2022-08-01 21:38:29 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+creating structs and using them
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for a dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Discription: a structure used to hold the name, age, and owner of a dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
