d8208e9aba1cf7ecc75a3cd12be6876e86837cea
Victor B. Onakomaiya <bolu.onako.1000@gmail.comgggit config --global user.email bolu.onako.1000@gmail.com>
2022-08-01 22:09:30 -0700

    1-init_dog.c corr1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	char *na = name;
+	float a = age;
+	char *own = owner;
+
+	(*d).name = na;
+	(*d).age = a;
+	(*d).owner = own;
 }
