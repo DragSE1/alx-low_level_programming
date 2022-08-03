ad86209a70e948e7f386378fb0bea2781d973c6b
Victor B. Onakomaiya <bolu.onako.1000@gmail.comgggit config --global user.email bolu.onako.1000@gmail.com>
2022-08-02 18:35:34 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -13,6 +14,14 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(struct dog));
 
-	*d = (struct dog) {.name = *name, .age = age, .owner = *owner}
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+
+	free(d);
 }
